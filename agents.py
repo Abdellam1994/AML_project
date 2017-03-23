d289b11aceb46e4bce834a0a3c5a4eac57a3b23b
Abdellah Lamrani Alaoui <abdellam1994@gmail.com>
2017-03-23 11:00:08 +0100

    minor modifications

--- agents.py
+++ agents.py
@@ -3,55 +3,50 @@ import operator as op
 from constants import *
 
 
-
-
-
-	
-
 class minAgent:
     # Here there is no need for parameters as this agent is a deterministic one
     def __init__(self):
-        self.revo = 0
+        self.revolution = 0
         return
 
     # There is also no need to update the state
-    def updateState(self, last, cards, history, revo, counter):
-        self.revo = revo
+    def updateState(self, last, cards, history, revolution, counter):
+        self.revolution = revolution
 
     # Here we choose the simple strategy of throwing the lowest value possible with the highest number possible
     def choose(self, pm):
         if len(pm) > 1:
-            values = np.array([[ranks[self.revo][move[0]], move[1]] for move in pm if move != (0, 0)])
+            values = np.array([[ranks[self.revolution][move[0]], move[1]] for move in pm if move != (0, 0)])
             card = np.min(values[:, 0])
             inds = (values[:, 0] == card)
-            move = (rev_ranks[self.revo][card], np.max(values[inds, 1]))
+            move = (rev_ranks[self.revolution][card], np.max(values[inds, 1]))
             return move
         else:
             return (0, 0)
 
-    # We update the revolution
-    def update(self, reward, last, cards, history, revo, moves, counter):
-        self.updateState(last, cards, history, revo, counter)
+    # We update the revolutionlution
+    def update(self, reward, last, cards, history, revolution, moves, counter):
+        self.updateState(last, cards, history, revolution, counter)
         return
 
 
 class maxAgent:
     # Here there is no need for parameters as this agent is a deterministic one
     def __init__(self):
-        self.revo = 0
+        self.revolution = 0
         return
 
     # There is also no need to update the state
-    def updateState(self, last, cards, history, revo, counter):
-        self.revo = revo
+    def updateState(self, last, cards, history, revolution, counter):
+        self.revolution = revolution
 
     # Here we choose the simple strategy of throwing the lowest value possible with the highest number possible
     def choose(self, pm):
         if len(pm) > 1:
-            values = np.array([[ranks[self.revo][move[0]], move[1]] for move in pm if move != (0, 0)])
+            values = np.array([[ranks[self.revolution][move[0]], move[1]] for move in pm if move != (0, 0)])
             card = np.max(values[:, 0])
             inds = (values[:, 0] == card)
-            move = (rev_ranks[self.revo][card], np.max(values[inds, 1]))
+            move = (rev_ranks[self.revolution][card], np.max(values[inds, 1]))
             return move
         else:
             return (0, 0)
@@ -60,11 +55,11 @@ class maxAgent:
 class RealPlayer:
     def __init__(self):
         self.last = (0, 0)
-        self.revo = 0
+        self.revolution = 0
         return
 
-    def updateState(self, last, cards, history, revo, counter):
-        self.revo = revo
+    def updateState(self, last, cards, history, revolution, counter):
+        self.revolution = revolution
         self.last = last
         return
 
@@ -99,43 +94,43 @@ class RealPlayer:
         else:
             return (0, 0)
 
-    # We update the revolution
-    def update(self, reward, last, cards, history, revo, moves, counter):
-        self.updateState(last, cards, history, revo, counter)
+    # We update the revolutionlution
+    def update(self, reward, last, cards, history, revolution, moves, counter):
+        self.updateState(last, cards, history, revolution, counter)
         return
 
 
 class minMaxAgent:
     # Here there is no need for parameters as this agent is a deterministic one
     def __init__(self):
-        self.revo = 0
+        self.revolution = 0
         self.minMax = 0
         return
 
     # There is also no need to update the state
-    def updateState(self, last, cards, history, revo, counter):
-        self.revo = revo
+    def updateState(self, last, cards, history, revolution, counter):
+        self.revolution = revolution
 
     # Here we choose the simple strategy of alternating between the lowest and largest values
     def choose(self, pm):
         if len(pm) > 1:
             if self.minMax == 0:
-                values = np.array([[ranks[self.revo][move[0]], move[1]] for move in pm if move != (0, 0)])
+                values = np.array([[ranks[self.revolution][move[0]], move[1]] for move in pm if move != (0, 0)])
                 card = np.min(values[:, 0])
                 inds = (values[:, 0] == card)
-                move = (rev_ranks[self.revo][card], np.max(values[inds, 1]))
+                move = (rev_ranks[self.revolution][card], np.max(values[inds, 1]))
                 return move
             else:
-                values = np.array([[ranks[self.revo][move[0]], move[1]] for move in pm if move != (0, 0)])
+                values = np.array([[ranks[self.revolution][move[0]], move[1]] for move in pm if move != (0, 0)])
                 card = np.max(values[:, 0])
                 inds = (values[:, 0] == card)
-                move = (rev_ranks[self.revo][card], np.max(values[inds, 1]))
+                move = (rev_ranks[self.revolution][card], np.max(values[inds, 1]))
                 return move
         else:
             return (0, 0)
 
-    # We update the revolution
-    def update(self, reward, last, cards, history, revo, moves, counter):
+    # We update the revolutionlution
+    def update(self, reward, last, cards, history, revolution, moves, counter):
         self.minMax = 1 - self.minMax
-        self.updateState(last, cards, history, revo, counter)
+        self.updateState(last, cards, history, revolution, counter)
         return
