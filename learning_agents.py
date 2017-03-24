b689575ec4b5884bc436b7cbfce6e579a6c67abb
Abdellah Lamrani Alaoui <abdellam1994@gmail.com>
2017-03-24 23:25:44 +0100

    final version of NNQL, need to finalize UCB1 and LSTD

--- learning_agents.py
+++ learning_agents.py
@@ -65,37 +65,161 @@ class NNQL_Agent:
 	Algorithm, using a neural network as an approximation of the Qvalue
 	"""
 	
-	def __init__(self, number_players, order, numero_player, gamma):
+	def __init__(self, number_players, gamma = 0.2, learning_rate = 0.1, epsilon = 0.3):
 		self.revolution = 0
 		self.number_players = number_players
-		self.list_NN = [NN([(self.number_players  - 1) * 13 * 4 + 13, 1]) for i in xrange(52)]
-		#Order of the game
-		self.order = order
+		self.learning_rate = learning_rate
+		self.list_NN = [NN([5 * 13 + 13 + 1, 1], self.learning_rate) for i in xrange(4 * 13 + 1)]
 		#Rank of the learning agent
-		self.numero_player = numero_player
 		self.state = 0
 		# Parameter of learning for Neural Networks
 		self.gamma = gamma
-		self.Q_values = [0 for i in xrange(52)]
+		self.Q_values = [np.random.uniform() for i in xrange(len(self.list_NN))]
 		self.action = 0
+		self.epsilon = epsilon
+		self.rewards = []
 		
 		
     # There is also no need to update the state
-	def updateState(self, last, hand, history, revolution, counter) :
+	def updateState(self, last, hand, history, revolution, counter, heuristics) :
 		self.revolution = revolution
-		proba = probabilities(hand, history, self.order, self.numero_player, revolution)
-		self.state = np.concatenate((np.array(hand), proba), axis = 0)
+		proba = probabilities(hand, history, revolution)
+		self.state = np.concatenate((np.array([hand.count(elem) for elem in values]), proba), axis = 0)
 		return None
 
-	def choose(self) :
-		self.action = np.argmax(Q_values)
-		# Needs to transform the action into the appropriate format
-		return tranform_inverse(self.action)
+	def choose(self, pm) :
+		
+		# Computing the possible action
+		possible_actions = [transform_state(move) for move in pm]
+		
+		if np.random.uniform() < self.epsilon :
+			self.action = possible_actions[np.random.randint(len(possible_actions))]
+		
+		else :
+			
+			# The Q values corresponding to the possible moves
+			Q_values_possible = [self.Q_values[i] if i in possible_actions else -100 for i in xrange(len(self.Q_values))]
+			self.action = np.argmax(Q_values_possible)
+			# Needs to transform the action into the appropriate format
+		return transform_inverse(self.action)
 
     # We update the revolution
-	def update(self, reward, last, hand, history, revolution, moves, counter):
-		self.updateState(last, hand, history, revolution, counter)
+	def update(self, reward, last, hand, history, revolution, moves, counter, heuristics):
+		self.updateState(last, hand, history, revolution, counter, heuristics)
 		# Computing the different Q_values
-		Q_values = [NN_.predict(self.state) for NN_ in self.list_NN]
-		self.list__NN[self.action].train(self.state, reward + self.gamma * Q_values[self.action])
+		self.Q_values = [NN_.predict(self.state)[0] for NN_ in self.list_NN]
+		self.list_NN[self.action].train(self.state, reward + self.gamma * self.Q_values[self.action])
+		return None
+		
+		
+		
+# Agent LSTD
+
+class LSTD_Agent:
+	
+	"""
+	Class that implements an agent whose decisions are based on the Q-Learning
+	Algorithm, using a neural network as an approximation of the Qvalue
+	"""
+	
+	def __init__(self, number_players, gamma = 0.2, learning_rate = 0.1, epsilon = 0.3):
+		self.revolution = 0
+		self.number_players = number_players
+		self.learning_rate = learning_rate
+		self.list_NN = [NN([5 * 13 + 13 + 1, 1], self.learning_rate) for i in xrange(4 * 13 + 1)]
+		#Rank of the learning agent
+		self.state = 0
+		# Parameter of learning for Neural Networks
+		self.gamma = gamma
+		self.Q_values = [np.random.uniform() for i in xrange(len(self.list_NN))]
+		self.action = 0
+		self.epsilon = epsilon
+		self.rewards = []
+		
+		
+    # There is also no need to update the state
+	def updateState(self, last, hand, history, revolution, counter, heuristics) :
+		self.revolution = revolution
+		proba = probabilities(hand, history, revolution)
+		self.state = np.concatenate((np.array([hand.count(elem) for elem in values]), proba), axis = 0)
+		return None
+
+	def choose(self, pm) :
+		
+		# Computing the possible action
+		possible_actions = [transform_state(move) for move in pm]
+		
+		if np.random.uniform() < self.epsilon :
+			self.action = possible_actions[np.random.randint(len(possible_actions))]
+		
+		else :
+			
+			# The Q values corresponding to the possible moves
+			Q_values_possible = [self.Q_values[i] if i in possible_actions else -100 for i in xrange(len(self.Q_values))]
+			self.action = np.argmax(Q_values_possible)
+			# Needs to transform the action into the appropriate format
+		return transform_inverse(self.action)
+
+    # We update the revolution
+	def update(self, reward, last, hand, history, revolution, moves, counter, heuristics):
+		self.updateState(last, hand, history, revolution, counter, heuristics)
+		# Computing the different Q_values
+		self.Q_values = [NN_.predict(self.state)[0] for NN_ in self.list_NN]
+		self.list_NN[self.action].train(self.state, reward + self.gamma * self.Q_values[self.action])
+		return None
+		
+		
+# Agent UCB1
+
+class UCB1_Agent:
+	
+	"""
+	Class that implements an agent whose decisions are based on the Q-Learning
+	Algorithm, using a neural network as an approximation of the Qvalue
+	"""
+	
+	def __init__(self, number_players, gamma = 0.2, learning_rate = 0.1, epsilon = 0.3):
+		self.revolution = 0
+		self.number_players = number_players
+		self.learning_rate = learning_rate
+		self.list_NN = [NN([5 * 13 + 13 + 1, 1], self.learning_rate) for i in xrange(4 * 13 + 1)]
+		#Rank of the learning agent
+		self.state = 0
+		# Parameter of learning for Neural Networks
+		self.gamma = gamma
+		self.Q_values = [np.random.uniform() for i in xrange(len(self.list_NN))]
+		self.action = 0
+		self.epsilon = epsilon
+		self.rewards = []
+		
+		
+    # There is also no need to update the state
+	def updateState(self, last, hand, history, revolution, counter, heuristics) :
+		self.revolution = revolution
+		proba = probabilities(hand, history, revolution)
+		self.state = np.concatenate((np.array([hand.count(elem) for elem in values]), proba), axis = 0)
+		return None
+
+	def choose(self, pm) :
+		
+		# Computing the possible action
+		possible_actions = [transform_state(move) for move in pm]
+		
+		if np.random.uniform() < self.epsilon :
+			self.action = possible_actions[np.random.randint(len(possible_actions))]
+		
+		else :
+			
+			# The Q values corresponding to the possible moves
+			Q_values_possible = [self.Q_values[i] if i in possible_actions else -100 for i in xrange(len(self.Q_values))]
+			self.action = np.argmax(Q_values_possible)
+			# Needs to transform the action into the appropriate format
+		return transform_inverse(self.action)
+
+    # We update the revolution
+	def update(self, reward, last, hand, history, revolution, moves, counter, heuristics):
+		self.updateState(last, hand, history, revolution, counter, heuristics)
+		# Computing the different Q_values
+		self.Q_values = [NN_.predict(self.state)[0] for NN_ in self.list_NN]
+		self.list_NN[self.action].train(self.state, reward + self.gamma * self.Q_values[self.action])
 		return None
